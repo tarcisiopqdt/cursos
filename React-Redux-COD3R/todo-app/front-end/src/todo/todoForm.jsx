73d1b7aeb623fa2ead574a8104d2986c3f6a8b7a
Paulo Tarcisio <paulo_tarcisio93@hotmail.com>
2019-03-20 22:54:44 -0300

    'form-component-added'

--- React-Redux-COD3R/todo-app/front-end/src/todo/todoForm.jsx
+++ React-Redux-COD3R/todo-app/front-end/src/todo/todoForm.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/IconButton'
 
 export default props => (
-    <div>
-        <h1>Form</h1>
+    <div role='form' className='todoForm'>
+
+        <Grid cols='12 9 10'>
+        <input id='decription' className='form-control' 
+            placeholder='Adicione uma tarefa' type="text" />
+        </Grid>
+          
+         <Grid cols='12 3 2'>
+            <IconButton style='danger' icon='plus' />
+        </Grid> 
+      
     </div>
 )
